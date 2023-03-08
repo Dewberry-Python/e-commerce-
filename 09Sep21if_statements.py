f75d282cb61bcd1803a22e60a3a7864cbc7c92f8
Constance189 <125757597+Constance189@users.noreply.github.com>
2023-03-08 21:42:45 +0200

    Add files via upload

---
+++ 09Sep21if_statements.py
@@ -0,0 +1,9 @@
+Play1= 44
+Play2= 49
+
+if Play1 > Play2:
+    print("The first number is greater than the second number")
+elif Play1 == Play2:
+    print("the two numbers are the same")
+else:
+    print("nothing is true here")
